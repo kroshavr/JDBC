abfb6eba0724b76cc90f126c669c699773d9953f
Kostya Rakhimzyanov <kroshavr@mail.ru>
2023-04-12 18:17:39 +0500

    1 commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,7 @@
+public class Application {
+    public static void main(String[] args) {
+        final String user = "postgres";
+        final String password = "Heso9m43";
+        final String url = "jdbc:postgresql://localhost:5432/skypro";
+    }
+}
